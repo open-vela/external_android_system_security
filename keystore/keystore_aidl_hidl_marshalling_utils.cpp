72f856117d0e3339b478c9c99c675b4d078ad797
liujinye <liujinye@xiaomi.com>
2018-12-05 10:48:08 +0800

    Use stream operator to report result code am: 7cb02e3c8e am: 3e5011b77b
    Change-Id: I76a2e6a0684374d80785f24a2c4925e723bf0a33

--- keystore/keystore_aidl_hidl_marshalling_utils.cpp
+++ keystore/keystore_aidl_hidl_marshalling_utils.cpp
@@ -219,7 +219,7 @@ status_t ExportResult::readFromParcel(const Parcel* inn) {
 }
 
 status_t ExportResult::writeToParcel(Parcel* out) const {
-    out->writeInt32(resultCode.getErrorCode());
+    out->writeInt32(resultCode);
     return keystore::writeKeymasterBlob(exportData, out);
 }
 
