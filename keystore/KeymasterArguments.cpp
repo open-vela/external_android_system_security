f9bc0b07d6959b0e7fbc65ec24edfafdc09742d3
liujinye <liujinye@xiaomi.com>
2019-01-30 04:04:58 +0800

    Snap for 5268384 from b7ff91bc0018848aa310bde61728264fe28c0274 to pi-qpr3-release
    Change-Id: I8a86f3f9651115c5da63ae78095f3039142f2ae0

--- keystore/KeymasterArguments.cpp
+++ keystore/KeymasterArguments.cpp
@@ -34,9 +34,6 @@ status_t KeymasterArguments::writeToParcel(android::Parcel* out) const {
     return keystore::writeParamSetToParcel(data_, out);
 };
 
-KeymasterArguments::KeymasterArguments(hardware::hidl_vec<keystore::KeyParameter>&& other)
-    : data_(std::move(other)) {}
-
 KeymasterArguments::KeymasterArguments(const hardware::hidl_vec<keystore::KeyParameter>& other)
     : data_(other) {}
 
