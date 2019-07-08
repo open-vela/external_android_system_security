55aafe30eeec2d2f5fdbd915b35e87ce2051327d
liujinye <liujinye@xiaomi.com>
2019-07-08 18:21:48 +0800

    Snap for 5600800 from 3a827b0a882258d291b67edbc5480e0ba0aedb39 to pi-platform-release
    Change-Id: I507fd16c59236a9fd0f69db82876c3cb4f623f4c

--- keystore/OperationResult.cpp
+++ keystore/OperationResult.cpp
@@ -46,19 +46,13 @@ status_t OperationResult::readFromParcel(const Parcel* inn) {
 }
 
 status_t OperationResult::writeToParcel(Parcel* out) const {
-    out->writeInt32(resultCode.getErrorCode());
+    out->writeInt32(resultCode);
     out->writeStrongBinder(token);
     out->writeInt64(handle);
     out->writeInt32(inputConsumed);
     keystore::writeKeymasterBlob(data, out);
     keystore::writeParamSetToParcel(outParams, out);
     return OK;
-}
-
-OperationResult operationFailed(const ::keystore::KeyStoreServiceReturnCode& error) {
-    OperationResult opResult = {};
-    opResult.resultCode = error;
-    return opResult;
 }
 
 }  // namespace keymaster
