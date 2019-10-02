cf1dc34e37f782b1029e832b737502bc8db6dddd
openvela-robot <openvela-robot@xiaomi.com>
2019-10-02 08:58:22 +0800

    Encrypt AES-256 keystore master keys.
    ag/5984229 that added support for AES-256 master keys inadvertently
    caused them not to be encyrpted by the user's password.  This is less
    damaging to security than it might appear because these keys are also
    encrypted by Keymaster, in the TEE or StrongBox.
    
    Bug: 141955555
    Test:  Manually verify password is encryption on a userdebug build.
    Change-Id: Ic5e82546df67346e4c348273cf4fe2bac382c9dc
    Merged-In: Ie44a4097e058bd5b9e45aa73115c266b9570a4fc

--- keystore/KeyAttestationApplicationId.cpp
+++ keystore/KeyAttestationApplicationId.cpp
@@ -31,9 +31,6 @@ KeyAttestationApplicationId::KeyAttestationApplicationId(
     packageInfos_->push_back(std::move(package));
 }
 
-KeyAttestationApplicationId::KeyAttestationApplicationId(PackageInfoVector packages)
-    : packageInfos_(std::make_shared<PackageInfoVector>(std::move(packages))) {}
-
 status_t KeyAttestationApplicationId::writeToParcel(Parcel* parcel) const {
     return parcel->writeParcelableVector(packageInfos_);
 }
